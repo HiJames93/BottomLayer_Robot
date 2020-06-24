7d80dda32a3db3265fb4d750d12babecfdc50628
HiJames93 <HiJames93@163.com>
2020-06-24 09:47:17 +0800

    fist commit

---
+++ src/main/java/cn/james/bottomlayerrobot/BottomlayerrobotApplication.java
@@ -0,0 +1,13 @@
+package cn.james.bottomlayerrobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BottomlayerrobotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BottomlayerrobotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/james/bottomlayerrobot/BottomlayerrobotApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.james.bottomlayerrobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BottomlayerrobotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
